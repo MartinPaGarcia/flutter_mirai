78be5fb07b335ff772eab0e66eff5f2e9d816c8e
MartinPaGarcia <a01066569@itesm.mx>
2021-10-18 23:47:02 -0500

    Add elevate buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import "package:flutter/material.dart";
 
 // void main() {
@@ -7,13 +9,41 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  void answerQuestion() {
+    // ignore: avoid_print
+    print("Anser chosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable
+    var questions = [
+      "What is your favourite colour?",
+      "What is your favourite animal?",
+    ]; // Questions
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("My First app"),
+        appBar: AppBar(
+          title: const Text("My First app"),
         ),
-        body:const Text("This is my default text!"),
+        body: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const Text("The question!"),
+            ElevatedButton(
+              child: Text("Answer1"),
+              onPressed: answerQuestion,
+              ),
+            ElevatedButton(
+              child: Text("Answer2"),
+              onPressed: answerQuestion,
+              ),
+            ElevatedButton(
+              child: Text("Answer3"),
+              onPressed: answerQuestion,
+              ),
+          ],
+        ),
       ),
     );
   }
