aaa27d3b5924643a1111e504cf9d0e6c3f1cc675
MartinPaGarcia <a01066569@itesm.mx>
2021-10-21 12:34:44 -0500

    Void callback and select handler

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,18 +1,20 @@
-// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, deprecated_member_use
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, deprecated_member_use, prefer_const_constructors_in_immutables, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  const Answer({Key? key}) : super(key: key);
-
+  final VoidCallback selectHandler;
+  Answer(this.selectHandler);
   @override
   Widget build(BuildContext context) {
     return Container(
-      width:double.infinity,
-      child:RaisedButton(
-        color:Colors.greenAccent,
-        child: Text("Answer1"),
-        onPressed: null,
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.greenAccent,
+        textColor: Colors.white70,
+        child: Text("Answer 1"),
+        //tambien puede usarse asi onPressed: () => selectHandler(),
+        onPressed: selectHandler,
       ),
     );
   }
